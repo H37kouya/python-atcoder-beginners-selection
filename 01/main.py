a13922c6952803b155998cb54479d208a87bf73d
H37kouya <h37kouya@gmail.com>
2021-06-11 21:27:01 +0900

    first commit

---
+++ 01/main.py
@@ -0,0 +1,14 @@
+
+def main() -> None:
+    # 整数の入力
+    a = int(input())
+    # スペース区切りの整数の入力
+    b, c = map(int, input().split())
+    # 文字列の入力
+    s = input()
+    # 出力
+    print("{} {}".format( a + b +c, s))
+
+
+if __name__ == "__main__":
+    main()
